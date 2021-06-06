c758967d5fe70fd5557b2a3d6d671e044191235a
Alexandrehs <echo.alexandre@gmail.com>
2021-06-06 15:13:12 -0300

    adicionado no arquivo ormconfig.js a opcao ssl:rejectUnauthorized para solucionar erro de self signde certificate

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,9 @@
 module.exports = {
     "type": "postgres",
-    "url": process.env.DATABASE_URL + "?sslmode=require",
+    "url": process.env.DATABASE_URL,
+    "ssl": true,
     "ssl": {
+        "require": true,
         "rejectUnauthorized": false
     },
     "migrations": [
